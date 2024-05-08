6dd1ff484e311be877dc0fac21f527df7e3de848
Yago Santos <yago@desktop.com>
2024-05-08 15:54:33 +0100

    update

--- lib/controllers/team_controller.dart
+++ lib/controllers/team_controller.dart
@@ -32,7 +32,7 @@ class TeamController {
     if (index == 1) {
       return 'Time ${index + 1} - CAMISA PBN/BENFICA/COLETE LARANJA - Score: ${team.score.toStringAsFixed(2)}';
     } else {
-      if (index == 0 && teamsLength == 2) {
+      if (index == 0 && teamsLength == 2 || index == 2 && teamsLength == 3 ) {
         return 'Time CAMISA PRETA - Score: ${team.score.toStringAsFixed(2)}';
       } else if (index == 0 && teamsLength == 3) {
         return 'Time ${index + 1} - CAMISA AMARELA - Score: ${team.score.toStringAsFixed(2)}';
